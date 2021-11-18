379da41cf5e7e4dafc7e5fddc43c59105a6dc69b
Brad Cypert <brad.cypert@gmail.com>
2021-11-18 18:11:26 -0500

    Validate positional args

--- cmd/root.go
+++ cmd/root.go
@@ -29,12 +29,21 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "ezzip",
 	Short: "Zip a directory, Unzip a zip file, encryption optional",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Zip a directory, Unzip a zip file, encryption optional
+	
+	zip a directory: 
+		ezzip my_dir
+	unzip a directory: 
+		ezzip my_dir.zip
+	
+	encrypt and zip:
+		ezzip my_dir --encrypt
+		...
+		use key: abc123 to decrypt
+	
+	unzip and decrypt:
+		ezzip my_dir.zip --key=abc123`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 
 		// TODO: What causes this to error?
@@ -50,7 +59,6 @@ to quickly create a Cobra application.`,
 
 			if key != nil {
 				fmt.Println("Use key: " + *key + " to decrypt")
-
 			}
 		}
 	},
